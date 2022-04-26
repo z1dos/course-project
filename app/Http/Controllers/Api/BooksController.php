2d04efcdef3853a316eb290b96d0c24b730e004b
z1dos <georg.awdeew2017@yandex.ru>
2022-04-26 18:23:26 +0700

    контроллер для выводп всех книг и ее роут

---
+++ app/Http/Controllers/Api/BooksController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    public function viewAllBook()
+    {
+        return Book::all();
+    }
+}
