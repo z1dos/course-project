255af9d4cf50ff337f718c58a157ce10099aa310
z1dos <georg.awdeew2017@yandex.ru>
2022-04-26 18:10:51 +0700

    Модели БД

---
+++ database/migrations/2022_04_26_073620_create_authors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('author_surname');
+            $table->string('author_name');
+            $table->string('author_patronymic');
+            $table->date('date_of_birth');
+            $table->date('date_of_death');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+};
---
+++ database/migrations/2022_04_26_073650_create_books_in_the_profiles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books_in_the_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('books_id')->constrained();
+            $table->foreignId('users_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books_in_the_profiles');
+    }
+};
