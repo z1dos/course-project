9439a067eb4cc1065c983bf70021b59e1dfb0cdf
z1dos <georg.awdeew2017@yandex.ru>
2022-04-27 22:15:04 +0700

    Добавление удаления Users

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -50,7 +50,8 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+        $user->update($request->all());
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/users', [\App\Http\Controllers\Api\UserController::class, 'index']);
+Route::get('/user/{id}', [\App\Http\Controllers\Api\UserController::class, 'show']);
+Route::put('/updateUser/{id}', [\App\Http\Controllers\Api\UserController::class, 'update']);
+
 Route::apiResources([
     'books' => \App\Http\Controllers\Api\BooksController::class,
     'authors' => \App\Http\Controllers\Api\AuthorsController::class,
     'genres' => \App\Http\Controllers\Api\GenreController::class,
-    'users' => \App\Http\Controllers\Api\UserController::class,
     'feedback' => \App\Http\Controllers\Api\FeedbackController::class,
     'estimation' => \App\Http\Controllers\Api\EstimationController::class,
     'profile' => \App\Http\Controllers\Api\BookInTheProfileController::class,
