265f41b775549102add32d806cb2fd4088205a19
Авдеев Георгий Анатольевич <gr403_avgan@ttit.local>
2022-04-28 17:09:37 +0700

    recource genre

--- app/Http/Resources/GenreResource.php
+++ app/Http/Resources/GenreResource.php
@@ -9,14 +9,14 @@ class GenreResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'genre_title ' => $this-> genre_title,
+            'genre_title' => $this->genre_title,
         ];
     }
 }
